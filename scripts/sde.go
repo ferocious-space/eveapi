b464877f02e92acc58b408aedbc9f40ba00d1c77
Ferocious Bite <ferocious.bite.eve@gmail.com>
2021-05-22 20:52:38 +0300

    update corporation endpoint

--- scripts/sde.go
+++ scripts/sde.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"reflect"
+	"runtime"
 	"strings"
 
 	"github.com/go-openapi/inflect"
@@ -19,10 +21,36 @@ func sdegen() {
 	if err != nil {
 		return
 	}
+	regionYaml := make([]reflect.Type, 0)
+	constellationYaml := make([]reflect.Type, 0)
+	solarsystemYaml := make([]reflect.Type, 0)
 	if err := filepath.Walk(
 		"./testing", func(path string, info fs.FileInfo, err error) error {
+			runtime.GC()
 			if info.IsDir() {
 				return nil
+			}
+			fmt.Println("Processing", filepath.Base(path))
+			if filepath.Base(path) == "region.staticdata" {
+				file, err := yamlparser.ParseFile(path)
+				if err != nil {
+					return err
+				}
+				regionYaml = append(regionYaml, file)
+			}
+			if filepath.Base(path) == "constellation.staticdata" {
+				file, err := yamlparser.ParseFile(path)
+				if err != nil {
+					return err
+				}
+				constellationYaml = append(constellationYaml, file)
+			}
+			if filepath.Base(path) == "solarsystem.staticdata" {
+				file, err := yamlparser.ParseFile(path)
+				if err != nil {
+					return err
+				}
+				solarsystemYaml = append(solarsystemYaml, file)
 			}
 			if filepath.Ext(path) != ".yaml" {
 				return nil
@@ -58,8 +86,37 @@ func sdegen() {
 			return nil
 		},
 	); err != nil {
-		fmt.Println("waling", err.Error())
+		fmt.Println("walking", err.Error())
 		return
 	}
-	return
+	regionType, err := yamlparser.GenerateType(yamlparser.DeepMergeList(regionYaml), "Regions", "github.com/fericous-space/eveapi/sde", "sde")
+	if err != nil {
+		return
+	}
+	if err := os.WriteFile(
+		filepath.Join("./sde", "Regions.go"), regionType,
+		os.ModePerm,
+	); err != nil {
+		return
+	}
+	constellationType, err := yamlparser.GenerateType(yamlparser.DeepMergeList(constellationYaml), "Constellations", "github.com/fericous-space/eveapi/sde", "sde")
+	if err != nil {
+		return
+	}
+	if err := os.WriteFile(
+		filepath.Join("./sde", "Constellations.go"), constellationType,
+		os.ModePerm,
+	); err != nil {
+		return
+	}
+	solarsystemType, err := yamlparser.GenerateType(yamlparser.DeepMergeList(solarsystemYaml), "Solarsystems", "github.com/fericous-space/eveapi/sde", "sde")
+	if err != nil {
+		return
+	}
+	if err := os.WriteFile(
+		filepath.Join("./sde", "Solarsystems.go"), solarsystemType,
+		os.ModePerm,
+	); err != nil {
+		return
+	}
 }
